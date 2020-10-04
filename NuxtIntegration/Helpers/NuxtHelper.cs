477b643f2599d395fbaf6a1695e713ba62408c83
Sam Walpole <walpoles93@googlemail.com>
2020-10-04 10:46:10 +0100

    Add project files.

---
+++ NuxtIntegration/Helpers/NuxtHelper.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace NuxtIntegration.Helpers
+{
+    public static class NuxtHelper
+    {
+        // default port number of 'npm run dev'
+        private static int Port { get; } = 3000;
+        private static Uri DevelopmentServerEndpoint { get; } = new Uri($"http://localhost:{Port}");
+        private static TimeSpan Timeout { get; } = TimeSpan.FromSeconds(30);
+        // done message of 'npm run dev' command.
+        private static string DoneMessage { get; } = "DONE  Compiled successfully in";
+
+        public static void UseNuxtDevelopmentServer(this ISpaBuilder spa)
+        {
+            spa.UseProxyToSpaDevelopmentServer(async () =>
+            {
+                var loggerFactory = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger("Nuxt");
+                // if 'npm dev' command was executed yourself, then just return the endpoint.
+                if (IsRunning())
+                {
+                    return DevelopmentServerEndpoint;
+                }
+
+                // launch Nuxt development server
+                var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = isWindows ? "cmd" : "npm",
+                    Arguments = $"{(isWindows ? "/c npm " : "")}run dev",
+                    WorkingDirectory = "client-app",
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                };
+                var process = Process.Start(processInfo);
+                var tcs = new TaskCompletionSource<int>();
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardOutput.ReadLine()) != null)
+                        {
+                            logger.LogInformation(line);
+                            if (!tcs.Task.IsCompleted && line.Contains(DoneMessage))
+                            {
+                                tcs.SetResult(1);
+                            }
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
+                    }
+                });
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardError.ReadLine()) != null)
+                        {
+                            logger.LogError(line);
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
+                    }
+                });
+
+                var timeout = Task.Delay(Timeout);
+                if (await Task.WhenAny(timeout, tcs.Task) == timeout)
+                {
+                    throw new TimeoutException();
+                }
+
+                return DevelopmentServerEndpoint;
+            });
+
+        }
+
+        private static bool IsRunning() => IPGlobalProperties.GetIPGlobalProperties()
+                .GetActiveTcpListeners()
+                .Select(x => x.Port)
+                .Contains(Port);
+    }
+}
---
+++ NuxtIntegration/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NuxtIntegration.Helpers;
+
+namespace NuxtIntegration
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSpaStaticFiles(options => options.RootPath = "client-app/dist");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client-app";
+                if (env.IsDevelopment())
+                {
+                    // Launch development server for Nuxt
+                    spa.UseNuxtDevelopmentServer();
+                }
+            });
+        }
+    }
+}
